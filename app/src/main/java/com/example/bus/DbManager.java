ab3aa98a508300401929a48d3b9713dcd4d33ce6
lunatic-frin9e <shubh@Shubhams-MacBook-Air.local>
2022-09-09 18:08:33 +0530

    Complete Project

---
+++ app/src/main/java/com/example/bus/DbManager.java
@@ -0,0 +1,53 @@
+package com.example.bus;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+
+public class DbManager extends SQLiteOpenHelper
+{
+    private static final String dbname="StudentDesc.db";
+
+    public DbManager(Context context)
+    {
+        super(context, dbname, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db)
+    {
+        String qry="create table tbl_student (id integer primary key autoincrement, name text, enroll text)";
+        db.execSQL(qry);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion)
+    {
+        db.execSQL("DROP TABLE IF EXISTS tbl_student");
+        onCreate(db);
+    }
+
+    public String addRecord(String p1, String p2)
+    {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+        cv.put("name",p1);
+        cv.put("Enroll",p2);
+
+        long res = db.insert("tbl_student",null,cv);
+
+        if(res == -1)
+        {
+            return "Failed";
+        }
+
+        else
+        {
+            return "Successfully inserted";
+        }
+    }
+}
